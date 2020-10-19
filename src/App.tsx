a767c8d693af53ce16f03b0465ff1cde7373c06c
bahalbach <bahalbach@gmail.com>
2020-10-19 19:03:30 +0800

    Added undo and redo

--- src/App.tsx
+++ src/App.tsx
@@ -2,102 +2,199 @@ import React, { useState } from 'react';
 import { nanoid } from 'nanoid'
 import './App.css';
 
-interface Node {id: string; text: string }
-interface ChildRelation {parentId: string; childId: string}
+interface Node { id: string; text: string; desc: string }
+interface ChildRelation { parentId: string; childId: string }
+enum EditModes {
+  None,
+  Title,
+  Description
+}
 
 const defaultId = nanoid();
-const defaultText = "Double Click to Edit";
-const defaultList: Node[] = [{ id: defaultId, text: defaultText }];
+const defaultText = "Title";
+const defaultDesc = ""
+const defaultList: Node[] = [];
 const defaultChildren: ChildRelation[] = [];
 
-/*
-  table 1: nodes
-  id, text
-
-  table 2: children
-  id, child id
-*/
-
-// function findParent(id: string) {
-//   const stack = [defaultList];
-//   var node;
-//   while (stack.length) {
-//     node = stack.shift();
-//     if (node?.id === id) {
-//       return node;
-//     }
-//     node?.children && stack.push(...node?.children)
-//   }
-//   return null;
-// }
-
-// function addChild(parentID: string) {
-//   const parent = findParent(parentID);
-//   if(parent) 
-//     parent.children = [{ id: nanoid(), text: defaultText, children: [] }, ...parent.children];
-//   console.log("added child")
-// }
-
-function ListItem(props: { nodeId: string; nodes: Node[], children: ChildRelation[], handleClick: any}) {
+function ListItem(props: { nodeId: string; nodes: Node[], children: ChildRelation[], addChild: any, removeThis: any }) {
   /*
     Displays text for this item, after the text there is a button to go into edit mode
     When clicked in normal mode expand child elements
   */
-  const text = props.nodes.find(node => node.id === props.nodeId)?.text;
-  const children = props.children.filter(relation => relation.parentId===props.nodeId);
+  const node = props.nodes.find(node => node.id === props.nodeId)
+  const text = node?.text;
+  const desc = node?.desc;
+  const children = props.children.filter(relation => relation.parentId === props.nodeId);
 
-  const [editMode, setEditMode] = useState(false);
-  const [content, setContent] = useState(text);
+  const [editMode, setEditMode] = useState(EditModes.None);
+  const [title, setTitle] = useState(text+props.nodeId);
+  const [description, setDescription] = useState(desc);
 
   return (
     <details className="ListItem">
-      <summary>
-        {editMode ?
+
+      <summary className="ListTitle">
+        {editMode === EditModes.Title ?
           <input
             type="text"
-            value={content}
+            value={title}
             autoFocus={true}
-            onBlur={() => setEditMode(false)}
-            onChange={function (e) { setContent(e.currentTarget.value) }}
+            onBlur={() => setEditMode(EditModes.None)}
+            onChange={function (e) { setTitle(e.currentTarget.value) }}
+            onFocus={function (e) {e.currentTarget.select()}}
           /> :
-          <span onDoubleClick={() => { setEditMode(true) }}> {content}  </span>
+          <span>
+            {title}
+            <button className="EditButton EditTitleButton" onClick={() => { setEditMode(EditModes.Title) }}>Edit</button>
+            <button className="EditButton RemoveTitleButton" onClick={props.removeThis(props.nodeId)}>Remove</button>
+          </span>
         }
       </summary>
+
       <div className="ListChildren">
-        {children.map(relation => 
-          <ListItem key={relation.childId} nodeId={relation.childId} nodes={props.nodes} children={props.children} handleClick={props.handleClick}/>
+
+        <div className="ListDescription">
+          {editMode === EditModes.Description ?
+            <textarea
+              value={description}
+              autoFocus={true}
+              onBlur={() => setEditMode(EditModes.None)}
+              onChange={function (e) { setDescription(e.currentTarget.value) }}
+            /> :
+            <div>
+              {description}
+              <br />
+              <button className="EditButton EditDescriptionButton" onClick={() => { setEditMode(EditModes.Description) }}>Edit</button>
+            </div>
+          }
+        </div>
+
+        {children.map(relation =>
+          <ListItem key={relation.childId} nodeId={relation.childId} nodes={props.nodes} children={props.children} addChild={props.addChild} removeThis={props.removeThis} />
         )}
-        <AddListItemButton onClick={ props.handleClick(props.nodeId) } />
+
+        <div>
+          <button onClick={props.addChild(props.nodeId)} className="AddChildButton">
+            +
+          </button>
+        </div>
       </div>
+
     </details>
   );
 }
 
-function AddListItemButton(props: { onClick: any }) {
+function SelectList() {
   return (
-    <div>
-      <button onClick={props.onClick}>
-        New List Item
-    </button>
-    </div>
+    <select>
+
+    </select>
   );
 }
 
 function App() {
-  const [nodes, setNodes] = useState(defaultList);
-  const [children, setChildren] = useState(defaultChildren);
+  const [historyIndex, setHistoryIndex] = useState(0);
+  const [nodeHistory, setNodeHistory] = useState([defaultList]);
+  const [childrenHistory, setChildrenHistory] = useState([defaultChildren]);
 
-  const handleClick = (parentId: string) => () => {
+  const [editMode, setEditMode] = useState(EditModes.None);
+  const [title, setTitle] = useState(defaultText);
+  const [description, setDescription] = useState(defaultDesc);
+
+  const nodes = nodeHistory[historyIndex];
+  const children = childrenHistory[historyIndex];
+  console.log("Nodes: ", nodes);
+  console.log("Children: ", children);
+
+  const addChild = (parentId: string) => () => {
     const childId = nanoid();
-    setNodes([...nodes, { id: childId, text: defaultText }]);
-    setChildren([...children, {parentId, childId}]);
+    setNodeHistory(nodeHistory.slice(0,historyIndex+1).concat([[...nodes, { id: childId, text: defaultText, desc: defaultDesc }]]));
+    setChildrenHistory(childrenHistory.slice(0,historyIndex+1).concat([[...children, { parentId, childId }]]));
+    setHistoryIndex(historyIndex + 1);
+  }
+
+  const removeThis = (nodeId: string) => () => {
+    let currentChildren = [nodeId];
+    let newNodes = nodeHistory[historyIndex];
+    let newChildren = childrenHistory[historyIndex];
+
+    let idsRemoved = 0;
+
+    while (currentChildren.length) {
+      let idsToRemove = currentChildren;
+      newNodes = newNodes.filter(node => !idsToRemove.includes(node.id));
+      newChildren = newChildren.filter(relation => !idsToRemove.includes(relation.childId));
+      currentChildren = children.filter(relation => idsToRemove.includes(relation.parentId)).map(relation => relation.childId);
+      idsRemoved += idsToRemove.length;
+    }
+    console.log("Removed ids: ", idsRemoved);
+
+    setNodeHistory(nodeHistory.slice(0,historyIndex+1).concat([newNodes]));
+    setChildrenHistory(childrenHistory.slice(0,historyIndex+1).concat([newChildren]));
+    setHistoryIndex(historyIndex + 1);
   }
 
   return (
     <div className="App">
-      Lists
-      <br />
-      <ListItem nodeId={defaultId} nodes={nodes} children={children} handleClick={handleClick}/>
+      <SelectList />
+      <summary className="ListTitle">
+        {editMode === EditModes.Title ?
+          <input
+            type="text"
+            value={title}
+            autoFocus={true}
+            onBlur={() => setEditMode(EditModes.None)}
+            onChange={function (e) { setTitle(e.currentTarget.value) }}
+            onFocus={function (e) {e.currentTarget.select()}}
+          /> :
+          <span>
+            {title}
+            <button className="EditButton EditTitleButton" onClick={() => { setEditMode(EditModes.Title) }}>Edit</button>
+            {/* <button className="EditButton RemoveTitleButton" onClick={props.removeThis(props.nodeId)}>Remove</button> */}
+          </span>
+        }
+      </summary>
+      <div className="ListDescription">
+        {editMode === EditModes.Description ?
+          <textarea
+            value={description}
+            autoFocus={true}
+            onBlur={() => setEditMode(EditModes.None)}
+            onChange={function (e) { setDescription(e.currentTarget.value) }}
+          /> :
+          <div>
+            {description}
+            <br />
+            <button className="EditButton EditDescriptionButton" onClick={() => { setEditMode(EditModes.Description) }}>Edit</button>
+          </div>
+        }
+      </div>
+
+      {children.filter(relation => relation.parentId === defaultId).map(relation =>
+        <ListItem key={relation.childId} nodeId={relation.childId} nodes={nodes} children={children} addChild={addChild} removeThis={removeThis} />
+      )}
+
+      <div>
+        <button onClick={addChild(defaultId)} className="AddChildButton">
+          +
+          </button>
+      </div>
+      <button
+        className="ControlButton UndoButton"
+        onClick={() => {
+          console.log("historyIndex: ", historyIndex);
+          console.log("nodeHistory.length: ", nodeHistory.length);
+          if (historyIndex > 0) setHistoryIndex(historyIndex - 1)
+        }}> Undo
+      </button>
+      <button
+        className="ControlButton RedoButton"
+        onClick={() => {
+          console.log("historyIndex: ", historyIndex);
+          console.log("nodeHistory.length: ", nodeHistory.length);
+          if (historyIndex < nodeHistory.length - 1) setHistoryIndex(historyIndex + 1)
+        }}> Redo
+      </button>
     </div>
   );
 }
