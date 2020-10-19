095e576c3369cadb4f15c86b137f3715b4890f48
bahalbach <bahalbach@gmail.com>
2020-10-19 12:02:55 +0800

    Created simple app

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,103 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { nanoid } from 'nanoid'
 import './App.css';
 
+interface Node {id: string; text: string }
+interface ChildRelation {parentId: string; childId: string}
+
+const defaultId = nanoid();
+const defaultText = "Double Click to Edit";
+const defaultList: Node[] = [{ id: defaultId, text: defaultText }];
+const defaultChildren: ChildRelation[] = [];
+
+/*
+  table 1: nodes
+  id, text
+
+  table 2: children
+  id, child id
+*/
+
+// function findParent(id: string) {
+//   const stack = [defaultList];
+//   var node;
+//   while (stack.length) {
+//     node = stack.shift();
+//     if (node?.id === id) {
+//       return node;
+//     }
+//     node?.children && stack.push(...node?.children)
+//   }
+//   return null;
+// }
+
+// function addChild(parentID: string) {
+//   const parent = findParent(parentID);
+//   if(parent) 
+//     parent.children = [{ id: nanoid(), text: defaultText, children: [] }, ...parent.children];
+//   console.log("added child")
+// }
+
+function ListItem(props: { nodeId: string; nodes: Node[], children: ChildRelation[], handleClick: any}) {
+  /*
+    Displays text for this item, after the text there is a button to go into edit mode
+    When clicked in normal mode expand child elements
+  */
+  const text = props.nodes.find(node => node.id === props.nodeId)?.text;
+  const children = props.children.filter(relation => relation.parentId===props.nodeId);
+
+  const [editMode, setEditMode] = useState(false);
+  const [content, setContent] = useState(text);
+
+  return (
+    <details className="ListItem">
+      <summary>
+        {editMode ?
+          <input
+            type="text"
+            value={content}
+            autoFocus={true}
+            onBlur={() => setEditMode(false)}
+            onChange={function (e) { setContent(e.currentTarget.value) }}
+          /> :
+          <span onDoubleClick={() => { setEditMode(true) }}> {content}  </span>
+        }
+      </summary>
+      <div className="ListChildren">
+        {children.map(relation => 
+          <ListItem key={relation.childId} nodeId={relation.childId} nodes={props.nodes} children={props.children} handleClick={props.handleClick}/>
+        )}
+        <AddListItemButton onClick={ props.handleClick(props.nodeId) } />
+      </div>
+    </details>
+  );
+}
+
+function AddListItemButton(props: { onClick: any }) {
+  return (
+    <div>
+      <button onClick={props.onClick}>
+        New List Item
+    </button>
+    </div>
+  );
+}
+
 function App() {
+  const [nodes, setNodes] = useState(defaultList);
+  const [children, setChildren] = useState(defaultChildren);
+
+  const handleClick = (parentId: string) => () => {
+    const childId = nanoid();
+    setNodes([...nodes, { id: childId, text: defaultText }]);
+    setChildren([...children, {parentId, childId}]);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Lists
+      <br />
+      <ListItem nodeId={defaultId} nodes={nodes} children={children} handleClick={handleClick}/>
     </div>
   );
 }
