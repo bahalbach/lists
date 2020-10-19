82189367346e73ec9b81611eb8b7b821b6677ce2
bahalbach <bahalbach@gmail.com>
2020-10-19 12:10:50 +0800

    Updates

---
+++ precache-manifest.d6be6e983d1e2442e79e32137b8130f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0a50f30c7483f5cbfdcd1097f980eb2",
+    "url": "/lists/index.html"
+  },
+  {
+    "revision": "c070649116006f7ada5c",
+    "url": "/lists/static/css/main.1fcd72a2.chunk.css"
+  },
+  {
+    "revision": "5043e82f27f40b69445e",
+    "url": "/lists/static/js/2.6fb2bbcf.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/lists/static/js/2.6fb2bbcf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c070649116006f7ada5c",
+    "url": "/lists/static/js/main.b1046041.chunk.js"
+  },
+  {
+    "revision": "7e9316747f13d25ba4e5",
+    "url": "/lists/static/js/runtime-main.ac8e194a.js"
+  }
+]);
