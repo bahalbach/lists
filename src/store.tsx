9852ad063016d91cfe899b0cfb0c73f3e9e6b60e
bahalbach <bahalbach@gmail.com>
2020-11-06 20:13:05 +0800

    saved data with local storage

--- src/store.tsx
+++ src/store.tsx
@@ -1,8 +1,24 @@
 import { configureStore } from "@reduxjs/toolkit";
 import listReducer from "./listSlice";
 
-export default configureStore({
+const stored = localStorage.getItem("reduxState");
+const preloadedState = stored ? { list: JSON.parse(stored) } : {};
+console.log("Loaded: ", stored);
+
+function saveState() {
+    let state = store.getState();
+    const stateString = JSON.stringify(state.list.present);
+    localStorage.setItem("reduxState", stateString);
+    console.log("Saved state", stateString)
+}
+
+const store = configureStore({
     reducer: {
         list: listReducer
-    }
-})
+    },
+    preloadedState
+});
+
+store.subscribe(() => saveState());
+
+export default store;
